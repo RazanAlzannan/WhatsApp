9a33374fa0224af871f967810ec8563c5ea211ef
Razan <razanalzannan@gmail.com>
2023-05-18 14:40:12 +0300

    Initial Commit

---
+++ WhatsApp/WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppApp.swift
+//  WhatsApp
+//
+//  Created by Razan Mohammed Alzannan on 28/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
